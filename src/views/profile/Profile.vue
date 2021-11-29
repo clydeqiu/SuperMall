ef0aa53d3f8beee76e067476ce4a649acd4ca5d1
qiuxra <qiuxra@yonyou.com>
2021-11-29 15:40:28 +0800

    fix: 我的-增加listView

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -11,7 +11,7 @@
     <!-- 2、暂时封装下 -->
     <Section :sectionInfo = 'sectionInfo'></Section>
 
-    <list-view :list-data="orderList" class="order-list"></list-view>
+    <list-view :list-data="orderList" class="order-list" @itemClick= "itemClick"></list-view>
     <list-view :list-data="serviceList" class="service-list"></list-view>
  </div>
 </template>
@@ -31,7 +31,16 @@ export default {
         balance: '100.00',
         discount: '3',
         rate: '1000'
-      }
+      },
+      orderList: [
+        {icon: '#order', iconColor: '#ff8198', info: '我的消息'},
+        {icon: '#point', iconColor: '#fc7b53', info: '积分商城'},
+        {icon: '#vip', iconColor: '#ffc636', info: '会员卡'},
+      ],
+      serviceList: [
+        {icon: '#service', iconColor: '#ff8198', info: '我的购物车'},
+        {icon: '#download', iconColor: '#ff8198', info: '下载购物APP'},
+      ]
     }
   },
   props: {
@@ -50,7 +59,9 @@ export default {
 
   },
   methods: {
-
+    itemClick(data) {
+      console.log(data);
+    }
   }
 }
 </script>
@@ -69,6 +80,6 @@ export default {
   }
 
   .order-list, .service-list {
-    margin-top: 12px;
+    margin-top: 8px;
   }
 </style>
--- src/views/profile/childComps/ListView.vue
+++ src/views/profile/childComps/ListView.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class='list-view'>
-    <h2>111</h2>
+  <div class='list' >
+    <div v-for= "(item, index) in listData" :key= "index" class= "item" @click="itemClick(index)">
+      <span class="icon">
+        <svg :fill= "item.iconColor">
+          <use :xlink:href= "item.icon"></use>
+        </svg>
+      </span>
+      <div class="info">{{item.info}}</div>
+    </div>
   </div>
 </template>
 
@@ -9,11 +16,15 @@ export default {
   name: 'ListView',
   data () {
     return {
-
     }
   },
   props: {
-
+    listData: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
   },
   components: {
 
@@ -25,13 +36,40 @@ export default {
 
   },
   methods: {
-
+    itemClick(index) {
+      this.$emit('itemClick',this.listData[index].info)
+    }
   }
 }
 </script>
 
 <style scoped>
-  .list-view{
+  .list {
     background-color: #fff;
+    font-size: 15px;
+    color: #333;
+  }
+  .list .item {
+    height: 44px;
+    line-height: 44px;
+    position: relative;
+  }
+  .icon svg {
+    width: 18px;
+    height: 18px;
+  }
+  .item .icon {
+    position: absolute;
+    top: 3px;
+    left: 16px;
+  }
+  .item .info {
+    margin-left: 40px;
+    border-bottom: 1px solid rgba(100, 100, 100, .1);
+    padding-left: 5px;
+  }
+
+  .item:last-of-type .info{
+    border-bottom: none;
   }
 </style>
